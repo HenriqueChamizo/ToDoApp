331042e46ccd9d4be44aab5212dc9c48d3c3a586
Henrique Chamizo <hchamizo@grupogbi.com>
2018-06-19 21:19:14 -0300

    add_begin_files

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from "./../template/pageHeader";
+
+export default props => {
+    return (
+        <div>
+            <PageHeader name="Sobre" small="Nós" />
+            <h2>Nossa Historia</h2>
+            <p>Hahahah hehehe. Hihihi hohoho huhuhuh</p>
+        </div>
+    )
+}
